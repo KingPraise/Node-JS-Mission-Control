1ac197d4571da6452b68213d8f498af724c044dc
King Praise <kingpraise15@gmail.com>
2025-07-17 09:07:41 +0100

    Getting Planet Data from Csv

--- server/src/server.js
+++ server/src/server.js
@@ -1,8 +1,20 @@
-const http = require('http');
-const app = require('./app');
+const http = require("http");
+const app = require("./app");
+const { loadPlanetsData } = require("./models/planets.model");
 const PORT = process.env.PORT || 8000;
-const server = http.createServer((app) => {});
+const server = http.createServer(app);
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+async function startServer() {
+  await loadPlanetsData()
+    .then(() => {
+      console.log("Planets data loaded successfully.");
+    })
+    .catch((error) => {
+      console.error("Error loading planets data:", error);
+    });
+  server.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
+}
+
+startServer();
